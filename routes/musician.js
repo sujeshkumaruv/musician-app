c64ef8adf2ac7d7d406d86de01e01b4ceb20ca63
Sujesh Kumar U V <sujeshkumaruv2@gmail.com>
2019-11-28 17:11:14 +0530

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: ok done done !");
 });
 
 // retrieve all musicians from data store
